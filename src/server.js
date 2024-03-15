015b5fa9c0bba9725d27c5308607e54d521f0fcc
Jireh-sama <jirehtumbagahan@gmail.com>
2024-03-15 10:07:42 +0800

    feat: add express server

---
+++ src/server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+const { processMessage } = require('./node_nlp/nlp');
+
+const app = express();
+const port = 3001;
+
+// Server Middleware
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+// Server route and handler
+app.get("/bot", async (req, res) => {
+
+  try {
+    const { message } = req.query;
+    
+    if (!message) {
+      return res.status(400).json({ error: 'Message is required' });
+    }
+
+    processMessage(message)
+        .then(result => {
+          console.log(result);
+          res.send(result.answer);
+          // res.json(result.answer);
+        })
+        .catch(error => {
+          res.status(500).json({ error: 'Error processing message' });
+        });
+  } 
+  catch (error) {
+    console.error("Error processing message:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// Start server
+function startServer() {
+  app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+  });
+}
+
+module.exports = startServer;
+ 
