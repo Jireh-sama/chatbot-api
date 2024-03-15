2ff3491e5074e7fddc18558fc636566cad53b34c
Jireh-sama <jirehtumbagahan@gmail.com>
2024-03-15 19:54:53 +0800

    feat: add json reader to manipulate json files

---
+++ src/jsonReader.js
@@ -0,0 +1,89 @@
+const fs = require("fs");
+
+const readJSONFile = (filePath) => {
+  try {
+    const data = fs.readFileSync(filePath, "utf8");
+    return JSON.parse(data);
+  } catch (err) {
+    console.error("Error reading json file:", err);
+    return null;
+  }
+};
+
+const updateJSONFile = (filePath, newData) => {
+  try {
+    // Read existing JSON data from file
+    const prevData = readJSONFile(filePath);
+  
+    const existingData = prevData.find(data => data.answer === newData.answer);
+  
+    if (!existingData) {
+      prevData.push(newData)
+      fs.writeFileSync(filePath, JSON.stringify(prevData));
+    } else {
+      existingData.frequency++;
+    }
+  
+    fs.writeFile(filePath, JSON.stringify(prevData, null, 2), (err) => {
+      if (err) {
+        console.error("Error writing file:", err);
+        return;
+      }
+      console.log("Attendance updated successfully.");
+    });
+    
+  } catch (error) {
+    console.error('Failed updating file ', error)
+  }
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const getObjectFromMatchingValue = (filePath, searchValue) => {
+  const jsonData = readJSONFile(filePath);
+
+  if (!jsonData) {
+    console.error("Unable to read JSON file");
+    return;
+  }
+
+  const foundObject = jsonData.find((obj) => obj.answer === searchValue);
+
+  if (!foundObject) {
+    console.log("No matching object Found");
+    return;
+  }
+
+  return foundObject;
+};
+
+module.exports = {
+  readJSONFile,
+  updateJSONFile,
+  getObjectFromMatchingValue,
+};
