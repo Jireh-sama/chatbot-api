f8219e37be4b0b3b4f3bda26a5a48d7d35cdb549
Jireh-sama <jirehtumbagahan@gmail.com>
2024-03-29 16:50:16 +0800

    feat(nlp): add module to match and return a value from given data sets

---
+++ src/node_nlp/feats/objectMatcher.js
@@ -0,0 +1,19 @@
+const { green, red } = require('colorette');
+const { getObjectFromMatchingValue } = require("../../jsonReader");
+
+const matchQuestionToObject = (searchValue, filePaths) => {
+
+  let foundObject = null;
+  for (let file of filePaths) {
+    foundObject = getObjectFromMatchingValue(file, searchValue);
+    if (foundObject) {
+      console.log(green('🔍 Found a value that matches the search value'));
+      return foundObject;
+    }
+  }
+  if (!foundObject) {
+    console.log(red('⚠️ No Matching value found'));
+  }
+};
+
+module.exports = matchQuestionToObject;
