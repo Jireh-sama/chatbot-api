605a8322514420b20f3b210c7853a1c1c1497017
Jireh-sama <jirehtumbagahan@gmail.com>
2024-03-29 16:36:32 +0800

    feat(firebase): create fn to insert data into the firestore database

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -3,7 +3,7 @@ const { getFirestore, collection, getDocs } = require('firebase/firestore');
 const admin = require('firebase-admin');
 
 // Initialize Firebase Admin SDK with the service account key file
-const serviceAccount = require('../serviceAccount/cob-chatbot-firebase-adminsdk-ix45m-b355fda3f7.json');
+const serviceAccount = require('./serviceAccount/cob-chatbot-firebase-adminsdk-ix45m-b355fda3f7.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -12,9 +12,31 @@ admin.initializeApp({
 // Get a reference to Firestore
 const db = admin.firestore();
 
+const insertData = async (collectionName, data, delPrevData=false) => {
+  try {
+    if (delPrevData) {
+      await db.collection(collectionName).get().then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          doc.ref.delete();
+        });
+      });
+    }
 
+    data.forEach((datum, index) => {
+      db.collection(collectionName).add(datum)
+        .then((docRef) => {
+          console.log(`Document ${index + 1} added with ID: ${docRef.id}`);
+        })
+        .catch((error) => {
+          console.error(`Error adding document ${index + 1}: `, error);
+        });
+    });
+  } catch (error) {
+    console.error("Failed inserting data to Database: ", error);
+  }
+}
 
-module.exports = async function fetchData(collectionName) {
+async function fetchData(collectionName) {
   try {
     // Reference to the collection you want to read from
     const dataRef = db.collection(collectionName);
@@ -40,5 +62,4 @@ module.exports = async function fetchData(collectionName) {
   }
 };
 
-// // Call the fetchData function to fetch data from Firestore
-// fetchData();
+module.exports = {insertData};
