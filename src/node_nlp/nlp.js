d646046688c72062274294aa1b9c6388e03bc1e3
Jireh-sama <jirehtumbagahan@gmail.com>
2024-04-05 13:53:31 +0800

    feat(nlp): update and add new fallback responses

--- src/node_nlp/nlp.js
+++ src/node_nlp/nlp.js
@@ -63,6 +63,34 @@ const getBaseIntent = (fullIntent) => {
 // Process a message using the NLP manager
 const processMessage = async (message) => {
   const response = await manager.process("en", message);
+
+  // ! Fallback Responses
+  utteranceLanguage = guessLanguage(response.utterance);
+  if (utteranceLanguage !== 'english' && utteranceLanguage !== 'tagalog') {
+    const fallbackResponses = [
+      "I'm sorry, I currently only support English. Can you please ask your question in English?",
+      "Apologies, my language skills are limited to English at the moment. Could you rephrase your question in English?",
+      "Unfortunately, I can only understand English right now. Could you please translate your query into English?",
+    ];
+    response.answer = fallbackResponses[Math.floor(Math.random() * fallbackResponses.length)];
+    return response;
+  } else if (utteranceLanguage === 'tagalog') {
+    // Do something specific for Tagalog language
+    response.answer = "Pasensya na, ngunit sa ngayon ay hindi ko pa kayang maunawaan ang Tagalog. Maaari mo bang itanong ang iyong katanungan sa Ingles?";
+    return response;
+  }
+  // ! Fallback Responses
+  responseIntent = response.intent.toLowerCase();
+  if (!response.answer || responseIntent === 'none') {
+    const fallbackResponses = [
+      "Oh, it seems I haven't quite gotten to that part in my learning journey yet! Is there something else I can help you with instead?",
+      "I'm sorry, I'm still learning and may not have the answer to that question just yet. Is there anything else I can assist you with?",
+      "Interesting! It seems I haven't learned about that just yet. What else can I assist you with?"
+    ]
+    response.answer = fallbackResponses[Math.floor(Math.random() * fallbackResponses.length)];
+    return response;
+  }
+
   const searchValue = response.answer;
 
   updateFrequency(searchValue);
