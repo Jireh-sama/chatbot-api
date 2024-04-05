ff6de292ce9d7d4744f1d4e36f6ad54befd8982d
Jireh-sama <jirehtumbagahan@gmail.com>
2024-04-05 14:02:11 +0800

    refactor: add and updated knowledge data to be trained to the model and some adjustments

--- src/node_nlp/feats/trainDataToModel.js
+++ src/node_nlp/feats/trainDataToModel.js
@@ -1,27 +1,39 @@
-const { yellow } = require('colorette');
-const greetingsData = require("../../knowledge/greetings_data.json");
-const questionData = require("../../knowledge/questions_data.json");
+const { yellow, red } = require('colorette');
+const salutationData = require('../../knowledge/salutation_data.json');
+const navigationData = require('../../knowledge/navigation_data.json');
+const questionData = require('../../knowledge/questions_data.json');
+const requestData = require('../../knowledge/request_data.json');
+const aboutBotData = require('../../knowledge/about_bot_data.json');
+
+
 let incrementingId = 1;
 
 const trainDataToModel = (manager) => {
 
   const loadDataIntoModel = (manager, data, incrementingId) => {
-    data.forEach((datum) => {
-      datum.documents.forEach((document) => {
-        manager.addDocument("en", document, datum.intent);
+    try {
+      data.forEach((datum) => {
+        datum.documents.forEach((document) => {
+          manager.addDocument("en", document, datum.intent);
+        });
+        manager.addAnswer("en", datum.intent, datum.answer, {
+          id: incrementingId++,
+        });
       });
-      manager.addAnswer("en", datum.intent, datum.answer, {
-        id: incrementingId++,
-      });
-    });
+    } catch (error) {
+      console.error('An error occured while loading data into the Model: ', error.message);
+    }
   };
-
-  if (greetingsData && questionData) {
+  const allData = [salutationData, questionData, navigationData, requestData, aboutBotData];
+  if (allData.every(data => data)) {
     console.log(yellow('Loading data into the model...'));
-    loadDataIntoModel(manager, greetingsData, incrementingId);
+    loadDataIntoModel(manager, salutationData, incrementingId);
     loadDataIntoModel(manager, questionData, incrementingId);
+    loadDataIntoModel(manager, navigationData, incrementingId);
+    loadDataIntoModel(manager, requestData, incrementingId);
+    loadDataIntoModel(manager, aboutBotData, incrementingId);
   } else {
-    console.error("Failed adding knowledge data to model");
+    console.error(red("One or more Knowledge data does not exist"));
   }
 }
 module.exports = trainDataToModel;
