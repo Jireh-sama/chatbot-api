d3f941513b92277550ab861a506383962fa6191a
Jireh-sama <jirehtumbagahan@gmail.com>
2024-04-12 17:34:32 +0800

    feat(api): add new routes to read, add and delete knowledge data

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cron = require('node-cron');
 const cors = require("cors");
+const { readJSONFile, getAllFilePaths, deleteJSONFile } = require('./utils/jsonReader');
 const { insertFAQsToDatabase } = require('./node_nlp/feats/manageFAQs');
-const { processMessage, getFrequentlyAskedQuestion, trainModel } = require('./node_nlp/nlp');
+const { createKnowledgeBase, insertKnowledgeTrainingData, deleteKnowledgeBase, deleteTrainingData } = require('./utils/knowledgeManager');
 
 const app = express();
 const port = 3001;
@@ -44,7 +45,78 @@ app.get('/bot/train', async (req, res) => {
   const status = await trainModel();
   console.log(status);
   res.send(status);
-}); 
+// route to get all knowledge data
+app.get('/bot/getKnowledge', async (req, res) => {
+  try {
+    console.log('gettings knowledge');
+    let allKnowledgeData = []
+    const filePaths = await getAllFilePaths('./knowledge');
+    for (const filePath of filePaths) {
+      const data = readJSONFile(filePath);
+      allKnowledgeData.push(data);
+    }
+    res.json(allKnowledgeData);
+  } catch (error) {
+    console.log('Errpr occured when getting all knowledege ', error);
+  }
+});
+// route to add a training data
+app.post('/bot/addTrainingData', async (req, res) => {
+  try {
+    const { knowledgeIndex, knowledgeName, ...trainingData } = req.body;
+    trainingData.documents = trainingData.documents.split(',').map(item => item.trim());
+    const filePaths = await getAllFilePaths('./knowledge');
+    if (knowledgeIndex < 0 || knowledgeIndex >= filePaths.length) {
+      throw new Error('Invalid knowledge index');
+    }
+    await insertKnowledgeTrainingData(filePaths[knowledgeIndex], trainingData)
+    console.log('Adding knowledge succesful');
+    res.status(200).json({ success: true, message: 'Training data added successfully' });
+  } catch (error) {
+    console.error('Addding knowledge fail: ', error);
+    res.status(404).json({ success: false, message: 'Failed adding training data' });
+  } 
+});
+// route to add a knowledge base
+app.post('/bot/addKnowledgeBase/', (req, res) => {
+  try {
+    const { knowledgeIndex, knowledgeName, ...trainingData } = req.body;
+    trainingData.documents = trainingData.documents.split(',').map(item => item.trim());
+    const pathTemplate = `./knowledge/${knowledgeName}.json`
+    console.log(pathTemplate);
+    console.log(trainingData);
+    createKnowledgeBase(pathTemplate, [trainingData]);
+    res.status(200).json({ success: true, message: 'Knowledge Base added successfully' });
+  } catch (error) {
+    console.log('error adding knowledge', error);
+    res.status(404).json({ success: false, message: 'Failed adding Knowledge Base' });
+  }
+});
+app.delete('/bot/deleteKnowledgeBase/',async (req, res) => {
+  try {
+    const { knowledgeBaseIndex } = req.body;
+    const filePaths = await getAllFilePaths('./knowledge');
+    const knowledgePath = filePaths[knowledgeBaseIndex]
+    deleteKnowledgeBase(knowledgePath)
+    res.status(200).json({ success: true, message: 'Knowledge Base deleted successfully' });
+  } catch (error) {
+    res.status(404).json({ success: false, message: 'Failed deleting Knowledge Base' });
+    
+  }
+});
+app.delete('/bot/deleteTrainingData/',async (req, res) => {
+  try {
+    const { knowledgeBaseIndex, dataIndex } = req.body;
+    const filePaths = await getAllFilePaths('./knowledge');
+    const knowledgePath = filePaths[knowledgeBaseIndex];
+    console.log(`We will delete training data from ${knowledgePath} with index ${dataIndex}`);
+    deleteTrainingData(knowledgePath, dataIndex)
+    res.status(200).json({ success: true, message: 'Knowledge Base deleted successfully' });
+  } catch (error) {
+    res.status(404).json({ success: false, message: 'Failed deleting Knowledge Base' });
+  }
+});
+
 
 // CRON TASKS
 /* 
