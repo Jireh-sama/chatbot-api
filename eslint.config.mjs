2391e09dee0be4261238edc0a2169e1a2d6b4e5e
Jireh <jirehtumbagahan@gmail.com>
2024-08-16 11:22:54 +0800

    chore: Update eslint and eslint-import-resolver-alias dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,31 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
+  // Add this section for path alias support
+  {
+    overrides: [
+      {
+        files: ["tests/**/*"],
+        env: {
+          jest: true,
+        },
+      },
+    ],
+    settings: {
+      "import/resolver": {
+        alias: {
+          map: [
+            ["@", "."],
+            ["@knowledge", "./src/knowledge/"],
+            ["@nlp", "./src/nlp/"],
+            ["@controllers", "./src/nlp/controllers/"],
+          ],
+          extensions: [".js", ".mjs", ".json"], // Add other extensions if necessary
+        },
+      },
+    },
+  },
 ];
