ae9f5ffe2a4aa96412dca2bc17b79ac3296db486
Jireh <jirehtumbagahan@gmail.com>
2024-08-17 10:08:41 +0800

    refactor: Update getAllKnowledgeData route to return success and error responses

--- core/routes/getAllKnowledgeData.js
+++ core/routes/getAllKnowledgeData.js
@@ -5,9 +5,11 @@ const router = express.Router()
 router.get('/bot/knowledge-data/', async (req, res) => {
   try {
     console.log('gettings knowledge');
-    res.json(await getAllKnowledge());
+    const allKnowledgeData = await getAllKnowledge()
+    return res.status(200).json({ success: true, knowledgeData: allKnowledgeData});
   } catch (error) {
     console.log('Error occured when getting all knowledege ', error);
+    return res.status(500).json({ success: false, message: 'Failed to get Knowledge Data Internal Server Error' })
   }
 })
 
