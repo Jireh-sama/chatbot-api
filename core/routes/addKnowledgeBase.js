4ec8ce0215beccfb96413df8219d809b87dfa12e
Jireh <jirehtumbagahan@gmail.com>
2024-08-17 10:08:24 +0800

    refactor: Improve error handling and logging in addKnowledgeBase route

--- core/routes/addKnowledgeBase.js
+++ core/routes/addKnowledgeBase.js
@@ -1,16 +1,25 @@
 const express = require('express')
+const { red, green } = require("colorette");
 const { createKnowledgeBase } = require('@nlp')
+const { checkKnowledgeBaseExists } = require('@validators/knowledgeValidator')
 const router = express.Router()
 
 router.post('/bot/knowledge-base/',async (req, res) => {
   try {
     const { knowledgeName, defaultDataEntry } = req.body;
+
+    const doesExist = await checkKnowledgeBaseExists(knowledgeName)
+    if (doesExist) {
+      console.error(red('Knowledge Base already exist'));
+      return res.status(400).json({ success: false, message: 'Knowledge Base already exist' });
+    }
     await createKnowledgeBase(knowledgeName, defaultDataEntry)
-    console.log('Knowledge Base added successfully');
-    res.status(200).json({ success: true, message: 'Knowledge Base added successfully' });
+    console.log(green('Knowledge Base added successfully'));
+    return res.status(200).json({ success: true, message: `Knowledge Base added successfully` });
   } catch (error) {
-    console.log('Error creating knowledge base', error);
-    res.status(500).json({ success: false, message: 'Failed to create knowledge base' });
+    const errorMessage = error.message || 'Failed to create knowledge base';
+    console.error(red(`Error creating knowledge base: ${errorMessage}`));
+    return res.status(500).json({ success: false, message: 'Failed to create knowledge base' });
   }
 });
 
