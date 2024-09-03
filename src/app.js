d3426aa35c2222228dcf666fd8adb03b5ad0b7ff
Jireh <jirehtumbagahan@gmail.com>
2024-09-03 19:08:48 +0800

    chore: Add validateRequestMiddleware to app.js

--- src/app.js
+++ src/app.js
@@ -3,18 +3,21 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import knowledgeRoutes from './interface/routes/knowledgeRoutes.js'
 import chatbotRoutes from './interface/routes/chatbotRoutes.js'
+import validateRequestMiddleware from './interface/middleware/validateRequestMiddleware.js';
+
 const port = process.env.PORT || 3001;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors({ origin: '*' }));
+app.use(validateRequestMiddleware)
 
 app.use('/api', knowledgeRoutes)
 app.use('/api', chatbotRoutes)
 
 const startServer = () => {
   app.listen(port, () => {
-    console.log(`🚀 Server is listening at http://localhost:${port}`);
+    console.log(`🚀 Server is listening on port: ${port}`);
   });
 }
 
