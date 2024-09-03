1b0305c08a5ee8b93f6464f23d8334ec25d94fa6
Jireh <jirehtumbagahan@gmail.com>
2024-09-03 17:36:14 +0800

    fix: Add query validation to processUserQuery function

--- src/application/use-cases/chatbot/processUserQuery.js
+++ src/application/use-cases/chatbot/processUserQuery.js
@@ -1,5 +1,8 @@
 function processUserQuery(chatbot) {
   const execute = async (query) => {
+    if (!query) {
+      throw new Error('A Query is required to process and deliver a response')
+    }
     const response = await chatbot.processUserQuery(query)
     // Do some logic to manipulating response here...
     return response;
