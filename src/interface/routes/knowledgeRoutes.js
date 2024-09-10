17d9fc7dbfe854519565e7adcadd107bddbb8a2a
Jireh <jirehtumbagahan@gmail.com>
2024-09-10 15:51:28 +0800

    feat: Add use cases and routes for knowledge oparations

--- src/interface/routes/knowledgeRoutes.js
+++ src/interface/routes/knowledgeRoutes.js
@@ -4,15 +4,15 @@ import KnowledgeController from "#interface/controllers/knowledgeController.js";
 import FileSystemStorage from "#infrastructure/persistence/database/storage/fileSystemStorage.js";
 
 // Use-Cases
-import CreateKnowledgeBase from "#application/use-cases/knowledge/createKnowledgeBase.js";
-import DeleteKnowledgeBase from "#application/use-cases/knowledge/deleteKnowledgeBase.js";
-import GetKnowledgeCollection from "#application/use-cases/knowledge/getKnowledgeCollection.js";
-import UpdateKnowledgeEntry from "#application/use-cases/knowledge/updateKnowledgeEntry.js";
-import DeleteKnowledgeEntry from "#application/use-cases/knowledge/deleteKnowledgeEntry.js";
-import AddKnowledgeEntry from "#application/use-cases/knowledge/addKnowledgeEntry.js";
+import CreateKnowledgeBase from "#application/use-cases/knowledge/create/createKnowledgeBase.js";
+import DeleteKnowledgeBase from "#application/use-cases/knowledge/delete/deleteKnowledgeBase.js";
+import GetKnowledgeCollection from "#application/use-cases/knowledge/read/getKnowledgeCollection.js";
+import GetKnowledgeEntry from "#application/use-cases/knowledge/read/getKnowledgeEntry.js";
+import UpdateKnowledgeEntry from "#application/use-cases/knowledge/update/updateKnowledgeEntry.js";
+import DeleteKnowledgeEntry from "#application/use-cases/knowledge/delete/deleteKnowledgeEntry.js";
+import AddKnowledgeEntry from "#application/use-cases/knowledge/create/addKnowledgeEntry.js";
+import DeleteKnowledgeEntryDocument from "#application/use-cases/knowledge/delete/deleteKnowledgeEntryDocument.js";
 
-const fileSystemStorage = FileSystemStorage()
-const knowledgeBaseRepository = KnowledgeBaseRepository(fileSystemStorage)
 
 const getKnowledgeCollection = GetKnowledgeCollection(knowledgeBaseRepository)
 const createKnowledgeBase = CreateKnowledgeBase(knowledgeBaseRepository)
@@ -24,10 +24,13 @@ const addKnowledgeEntry = AddKnowledgeEntry(knowledgeBaseRepository)
 
 const knowledgeController = KnowledgeController(
   getKnowledgeCollection,
+  getKnowledgeEntry,
+
   createKnowledgeBase,
   deleteKnowledgeBase,
   updateKnowledgeEntry,
   deleteKnowledgeEntry,
+  deleteKnowledgeEntryDocument,
   addKnowledgeEntry
 );
 
@@ -40,9 +43,11 @@ router.post('/knowledge/entry', (req, res) => knowledgeController.addKnowledgeEn
 router.put('/knowledge/entry', (req, res) => knowledgeController.updateKnowledgeEntry(req, res))
 
 router.get('/knowledge', (req, res) => knowledgeController.getKnowledgeCollection(req, res))
+router.get('/knowledge/:knowledgeBase', (req, res) => knowledgeController.getKnowledgeEntry(req, res))
+
 
 router.delete('/knowledge/entry', (req, res) => knowledgeController.deleteKnowledgeEntry(req, res))
+router.delete('/knowledge/entry/document', (req, res) => knowledgeController.deleteKnowledgeEntryDocument(req, res))
 router.delete('/knowledge/:knowledgeBaseName', (req, res) => knowledgeController.deleteKnowledgeBase(req, res))
-
 
 export default router
