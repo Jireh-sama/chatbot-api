805f3764a8fb512b414e8f9e2375977347f37f65
Jireh <jirehtumbagahan@gmail.com>
2024-09-10 15:56:30 +0800

    refactor: Remove unnecessary knowledge base validation function

--- src/domain/services/chatbotService.js
+++ src/domain/services/chatbotService.js
@@ -1,4 +1,3 @@
-import CreateKnowledgeEntry from "../entities/knowledgeEntry.js"
 /**
  * Encapsulates the logic for handling NLP-related tasks. 
  * It abstracts away the complexities of the node-nlp 
@@ -11,23 +10,11 @@ function chatbotService(chatbot, reader) {
     const response = await chatbot.process('en', query)
     return response;
   }
+
   const loadModel = (modelFilePath) => {
     chatbot.load(modelFilePath)
   }
 
-  const validateKnowledgeBase = (knowledgeBase) => {
-    if (!Array.isArray(knowledgeBase)) {
-      throw new Error(`Knowledge base must be an Array, received ${typeof knowledgeBase}`)
-    }
-    if (knowledgeBase.length === 0) {
-      throw new Error(`Knowledge base is empty`)
-    }
-    for (const knowledgeEntry of knowledgeBase) {
-      const {intent, documents, answer} = knowledgeEntry;
-      const createdEntry = CreateKnowledgeEntry(intent, documents, answer)
-      createdEntry.validate()
-    }
-  }
   const loadDataIntoModel = (knowledgeBase) => {
     try {
       for (const knowledgeEntry of knowledgeBase) {
@@ -41,11 +28,12 @@ function chatbotService(chatbot, reader) {
     }
   }
   const trainModel = async (knowledgeBasePathList, modelFilePath) => {
+    // Refactor this later
     for (const knowledgeBasePath of knowledgeBasePathList) {
       try {
         const knowledgeBase = await reader(knowledgeBasePath)
         console.log('Validating knowledge base: ',knowledgeBasePath);
-        validateKnowledgeBase(knowledgeBase)
+    
         console.log('Loading data into model: ',knowledgeBasePath);
         loadDataIntoModel(knowledgeBase)
       } catch (error) {
