5211e44f74b2a8dd1854ef8c74b86b16c6cee0a8
Jireh <jirehtumbagahan@gmail.com>
2024-09-11 16:31:22 +0800

    refactor: Remove old way adding knowledge entry

--- src/application/use-cases/knowledge/create/addKnowledgeEntry.js
+++ src/application/use-cases/knowledge/create/addKnowledgeEntry.js
@@ -1,20 +1,21 @@
 import createKnowledgeEntry from "#domain/entities/knowledgeEntry.js"
-import { defaultKnowledgeDirectory, defaultKnowledgeExtension } from "#infrastructure/config/paths.js"; 
-import { getFilePath } from "#infrastructure/utils/pathUtils.js";
 
 function addKnowledgeEntry(knowledgeRepository) {
-  const execute = async (knowledgeBaseName, knowledgeEntry) => {
+  const execute = async (knowledgeBase, knowledgeEntry) => {
+
+    const existingKnowledge = await knowledgeRepository.readKnowledgeBase(
+      knowledgeBase
+    );
+
+    if (!existingKnowledge) {
+      throw new Error(`Knowledge base ${knowledgeBase} does not exist`);
+    }
 
     const { intent, documents, answer } = knowledgeEntry 
     const newKnowledgeEntry = createKnowledgeEntry(intent, documents, answer)
     newKnowledgeEntry.validate()
 
-    const knowledgeBasePath = getFilePath(defaultKnowledgeDirectory, knowledgeBaseName, defaultKnowledgeExtension)
-    const selectedKnowledgeBase = await knowledgeRepository.readKnowledgeBase(knowledgeBasePath)
-
-    selectedKnowledgeBase.push(newKnowledgeEntry.toObject())
-
-    knowledgeRepository.updateKnowledgeBase(knowledgeBasePath, selectedKnowledgeBase)
+    knowledgeRepository.addKnowledgeEntry(knowledgeBase, newKnowledgeEntry.toObject())
   }
 
   return { execute }
