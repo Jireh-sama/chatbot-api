f6964b54cc1da23d926fb58747b489b38e25fc0d
Jireh <jirehtumbagahan@gmail.com>
2024-09-12 21:39:39 +0800

    refactor: update logging and res value

--- src/interface/controllers/chatbotController.js
+++ src/interface/controllers/chatbotController.js
@@ -4,8 +4,8 @@ function chatbotController(processUserQueryUseCase) {
     try {
       const { userQuery } = req.body;
       const response = await processUserQueryUseCase.execute(userQuery)
-      console.log(`User query successfully handled, Query: ${response}`);
-      return res.status(200).json({ success: true, response: response })
+      console.log(`User query successfully handled, Query: ${response.utterance}`);
+      return res.status(200).json({ success: true, response })
     } catch (error) {
       console.log(error.message || error);
       return res.status(500).json({ success: false, response: error.message || error })
