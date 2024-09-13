a616d78ab0dc1267b4c8d1e8fb5245b1bab8974b
Jireh <jirehtumbagahan@gmail.com>
2024-09-13 11:43:57 +0800

    chore: replace old admin panel static files

--- src/interface/middleware/validateRequestMiddleware.js
+++ src/interface/middleware/validateRequestMiddleware.js
@@ -1,9 +1,13 @@
 function validateRequestMiddleware(req, res, next) {
-  const apiKey = req.headers['authorization']
-  if (apiKey !== process.env.API_KEY) {
-    console.error('Unauthorized request has been detected');
-    return res.status(401).json({ success: false, response: 'Unauthorized Request' })
-  }
+
+  // if (req.path === '/admin') {
+  //   return next()
+  // }
+  // const apiKey = req.headers['authorization']
+  // if (apiKey !== process.env.API_KEY) {
+  //   console.error('Unauthorized request has been detected');
+  //   return res.status(401).json({ success: false, response: 'Unauthorized Request' })
+  // }
   return next()
 }
 
