f0422cf78a2600b94ae8f54d7b19f5b4fb34fd9f
Jireh <jirehtumbagahan@gmail.com>
2024-09-13 11:43:26 +0800

    feat: add route to serve static files

--- src/app.js
+++ src/app.js
@@ -4,16 +4,26 @@ import bodyParser from 'body-parser';
 import knowledgeRoutes from './interface/routes/knowledgeRoutes.js'
 import chatbotRoutes from './interface/routes/chatbotRoutes.js'
 import validateRequestMiddleware from './interface/middleware/validateRequestMiddleware.js';
+import path from 'path'
+import { fileURLToPath } from 'url';
+
+// Get the directory name of the current module
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const port = process.env.PORT || 3001;
 const app = express();
 
+console.log(__dirname);
+app.use(express.static(path.join(__dirname, 'static')));
 app.use(bodyParser.json());
 app.use(cors({ origin: '*' }));
 app.use(validateRequestMiddleware)
 
 app.use('/api', knowledgeRoutes)
 app.use('/api', chatbotRoutes)
+
 
 const startServer = () => {
   app.listen(port, () => {
