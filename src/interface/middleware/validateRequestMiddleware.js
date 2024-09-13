771721210a113429fd3e1e16326ffa69144b49e0
Jireh <jirehtumbagahan@gmail.com>
2024-09-13 11:46:19 +0800

    fix: uncomment validate middleware

--- src/interface/middleware/validateRequestMiddleware.js
+++ src/interface/middleware/validateRequestMiddleware.js
@@ -1,13 +1,9 @@
 function validateRequestMiddleware(req, res, next) {
-
-  // if (req.path === '/admin') {
-  //   return next()
-  // }
-  // const apiKey = req.headers['authorization']
-  // if (apiKey !== process.env.API_KEY) {
-  //   console.error('Unauthorized request has been detected');
-  //   return res.status(401).json({ success: false, response: 'Unauthorized Request' })
-  // }
+  const apiKey = req.headers['authorization']
+  if (apiKey !== process.env.API_KEY) {
+    console.error('Unauthorized request has been detected');
+    return res.status(401).json({ success: false, response: 'Unauthorized Request' })
+  }
   return next()
 }
 
