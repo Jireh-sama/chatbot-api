11c6644618f75767c31120ecaabefb5405baf6ff
Jireh <jirehtumbagahan@gmail.com>
2024-09-30 16:40:55 +0800

    refactor: remove checkAccessToken middleware

--- src/app.js
+++ src/app.js
@@ -3,9 +3,10 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import knowledgeRoutes from './interface/routes/knowledgeRoutes.js'
 import chatbotRoutes from './interface/routes/chatbotRoutes.js'
-import validateRequestMiddleware from './interface/middleware/validateRequestMiddleware.js';
+import authRoutes from './interface/routes/authRoutes.js';
 import path from 'path'
 import { fileURLToPath } from 'url';
+import validateRequestMiddleware from './interface/middleware/validateRequestMiddleware.js';
 
 // Get the directory name of the current module
 const __filename = fileURLToPath(import.meta.url);
@@ -14,23 +15,15 @@ const __dirname = path.dirname(__filename);
 const port = process.env.PORT || 3001;
 const app = express();
 
-function checkAccessToken(req, res, next) {
-  const { token } = req.params;
-  const allowedToken = '5b9SlUTT3l4lVvbf2HCcI4w9VU69VI8W3e1tbAMWBkXFGoqCkbhtklM5UfHmstES'
-  if (token !== allowedToken) {
-    return res.status(404).json({message: 'Nyope'})
-  }
-  return next()
-}
-
 
 app.use(bodyParser.json());
 app.use(cors({ origin: '*' }));
 
-app.use('/static/:token', checkAccessToken, express.static(path.join(__dirname, 'static')));
+app.use('/static/', express.static(path.join(__dirname, 'static')));
 
-app.use('/api', validateRequestMiddleware, knowledgeRoutes)
+app.use('/api', validateRequestMiddleware,knowledgeRoutes)
 app.use('/api', validateRequestMiddleware, chatbotRoutes)
+app.use('/api', validateRequestMiddleware, authRoutes)
 
 
 const startServer = () => {
