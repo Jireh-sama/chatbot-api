fa3a2f6712c86bc72be776d0852f2da414d4ac1d
Jireh <jirehtumbagahan@gmail.com>
2024-09-30 16:42:31 +0800

    refactor: add repostory and db client for user

--- src/infrastructure/service/index.js
+++ src/infrastructure/service/index.js
@@ -3,12 +3,16 @@
 import MongoDbClient from "#infrastructure/persistence/database/mongodb/client.js";
 import KnowledgeRepository from "#infrastructure/persistence/repositories/knowledgeRepository.js";
 import ChatbotClient from "#infrastructure/persistence/chatbot/client.js";
+import UserRepository from "#infrastructure/persistence/repositories/userRepository.js";
 import { uri, defaultMongoDbConfig, dbName, collectionName } from "#infrastructure/config/db.js";
 import { nlpManagerConfig } from "#infrastructure/config/nlpManagerConfig.js";
 import { modelFilePath } from "../config/paths.js";
 
-const mongoDbClient = MongoDbClient(uri, dbName, collectionName, defaultMongoDbConfig);
-const knowledgeRepository = KnowledgeRepository(mongoDbClient);
+const userDB = MongoDbClient(uri, dbName, 'user', defaultMongoDbConfig)
+const knowledgeDB = MongoDbClient(uri, dbName, collectionName, defaultMongoDbConfig);
+
+const knowledgeRepository = KnowledgeRepository(knowledgeDB);
+const userRepository = UserRepository(userDB)
 const chatbotClient = ChatbotClient(nlpManagerConfig, knowledgeRepository, modelFilePath)
 
-export { mongoDbClient, knowledgeRepository, chatbotClient };
+export { knowledgeRepository, chatbotClient, userRepository };
