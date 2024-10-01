e79f9c31153712857eecd794924037623f789f0e
Jireh <jirehtumbagahan@gmail.com>
2024-10-01 19:21:47 +0800

    chore: remove try-catch blocks as error handling now happens in the error handler middleware

--- src/interface/controllers/authController.js
+++ src/interface/controllers/authController.js
@@ -2,18 +2,15 @@ import { hashPassword } from "#src/infrastructure/utils/passwordUtils.js"
 
 function authController(authenticateAdminUseCase) {
   const loginAdmin = async (req, res) => {
-    try {
-      const { email, password } = req.body
-      await authenticateAdminUseCase.execute(email, password)
-      res.status(200).json({ message: 'Successfully logged in' })
-    } catch (error) {
-      res.status(500).json({ message: 'something went wrong' })
-      console.log(error);      
-    }
+    const { email, password } = req.body
+    await authenticateAdminUseCase.execute(email, password)
+    res.status(200).json({ message: 'Successfully logged in' })
   }
 
   const registerAdmin = async (req, res) => {
     const { email, password } = req.body
+
+    
     if (email) {
       throw new Error('Email exist')
     }
