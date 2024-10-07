84b4982ccaf257a7c382a09df9efc17f0232bd06
Jireh <jirehtumbagahan@gmail.com>
2024-10-07 20:19:01 +0800

    chore: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,24 +2,15 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
-  { files: ["**/*.js"], languageOptions: { sourceType: "module" } },
-  { languageOptions: { globals: globals.node } },
-  pluginJs.configs.recommended,
-  {
-    settings: {
-      "import/resolver": {
-        alias: {
-          map: [
-            ["@", "."],
-            ["@knowledge", "./src/knowledge/"],
-            ["@entities", "./src/entities/KnowledgeEntry.js"],
-            ["@nlp", "./src/nlp/"],
-            ["@validators", "./src/validators/"],
-            ["@controllers", "./src/nlp/controllers/"],
-          ],
-          extensions: [".js", ".mjs", ".json"],
-        },
-      },
-    },
+  { 
+    files: ["**/*.js"], 
+    languageOptions: { 
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+        CustomError: "readonly",
+      }
+    } 
   },
+  pluginJs.configs.recommended,
 ];
