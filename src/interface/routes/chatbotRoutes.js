8192090458211f7ac5d72927f00ab5fa76177ca2
Jireh <jirehtumbagahan@gmail.com>
2024-10-07 20:18:49 +0800

    refactor: wrap controller handler with async handler and use the auth middlewares

--- src/interface/routes/chatbotRoutes.js
+++ src/interface/routes/chatbotRoutes.js
@@ -1,10 +1,12 @@
+import { verifyToken, verifyAPIKey } from "../middleware/authMiddleware.js";
+import { asyncHandler } from "../middleware/errorHandler.js";
+
 import express from "express";
 import ChatbotController from "../controllers/chatbotController.js";
 import { chatbotClient } from "#src/infrastructure/service/index.js";
 
 import ProcessUserQuery from "#src/application/use-cases/chatbot/processUserQuery.js";
 import TrainChatbot from "#src/application/use-cases/chatbot/trainChatbot.js";
-import { asyncHandler } from "../middleware/errorHandler.js";
 
 const router = express.Router()
 
@@ -13,7 +15,8 @@ const trainChatbot = TrainChatbot(chatbotClient);
 
 const chatBotController = ChatbotController(processUserQuery, trainChatbot);
   
-router.post('/query', asyncHandler(chatBotController.processUserQuery))
-router.post('/train', asyncHandler(chatBotController.trainChatbot))
+router.post('/query', verifyAPIKey, asyncHandler(chatBotController.processUserQuery))
+router.post('/query-admin', verifyToken, asyncHandler(chatBotController.processUserQuery))
+router.post('/train', verifyToken, asyncHandler(chatBotController.trainChatbot))
 
 export default router
