62a0f96e6dad95fd723bd90646635c99acdc768b
Jireh <jirehtumbagahan@gmail.com>
2024-10-07 20:13:02 +0800

    refactor: add condition to specifically handle the custom error

--- src/interface/middleware/errorHandler.js
+++ src/interface/middleware/errorHandler.js
@@ -1,5 +1,17 @@
+
 export const globalErrorHandler = (err, req, res, next) => {
-  console.error(err); // Log the error for debugging
+  if (err instanceof CustomError) {
+    const statusCode = err.statusCode;
+    const message = err.message;
+
+    return res.status(statusCode).json({
+      success: false,
+      message: message,
+    });
+  }
+  if (process.env.NODE_ENV === 'development') {
+    console.error(err); // Log the error for debugging
+  }
 
   const statusCode = err.status || 500; // Set a default status code
   const message = err.message || 'Internal Server Error'; // Set a default message
