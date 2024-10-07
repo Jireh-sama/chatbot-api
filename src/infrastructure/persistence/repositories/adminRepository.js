aa5f34d5df37a51f15c9e4fadeb05710030eeb18
Jireh <jirehtumbagahan@gmail.com>
2024-10-07 20:12:28 +0800

    feat: add admin repository

--- src/infrastructure/persistence/repositories/adminRepository.js
+++ src/infrastructure/persistence/repositories/adminRepository.js
@@ -1,15 +1,27 @@
 function adminRepository(db) {
 
-  const findByEmail = async (email) => {
-    // logic to find user
+  const findOneAdmin = async (filter) => {
+    return await db.readCollection(filter, null, true)
   }
-  const createAdmin = async (user) => {
-    // logic to create admin
+
+  const findAllAdmin = async (email) => {
+    const query = { email }
+    const isSingle = true
+    return await db.readCollection(query, null, isSingle)
   }
+  const createAdmin = async (email, password) => {
+    const document = { email, password }
+    await db.addDocument(document)
+  }
+  const updateAdminRefreshToken = async (adminId, query) => {
+    await db.updateDocument({ _id: adminId }, query)
+  } 
   
   return {
-    findByEmail,
+    findOneAdmin,
+    findAllAdmin,
     createAdmin,
+    updateAdminRefreshToken,
   }
 }
 
