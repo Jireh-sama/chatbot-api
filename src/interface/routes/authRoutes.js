913cbdee1f9377b71aadcdad6455f359f93f2740
Jireh <jirehtumbagahan@gmail.com>
2024-10-07 20:18:10 +0800

    feat: update auth routes

--- src/interface/routes/authRoutes.js
+++ src/interface/routes/authRoutes.js
@@ -1,16 +1,26 @@
 import express from "express";
 import { adminRepository } from "#src/infrastructure/service/index.js";
+import { asyncHandler } from "../middleware/errorHandler.js";
 import AuthController from "../controllers/authController.js";
 
-import AuthenticateAdmin from "#src/application/use-cases/auth/authenticateAdmin.js";
-import { asyncHandler } from "../middleware/errorHandler.js";
+//Use-cases
+import CreateAdmin from "#src/application/use-cases/admin/createAdmin.js";
+import LoginAdmin from "#src/application/use-cases/admin/loginAdmin.js";
+import LogoutAdmin from "../../application/use-cases/admin/logoutAdmin.js";
+import IssueAdminNewAccessToken from "../../application/use-cases/admin/issueAdminNewAccessToken.js";
 
-const authenticateAdmin = AuthenticateAdmin(adminRepository)
-const authController = AuthController(authenticateAdmin);
+const createAdmin = CreateAdmin(adminRepository)
+const loginAdmin = LoginAdmin(adminRepository)
+const logoutAdmin = LogoutAdmin(adminRepository)
+const issueAdminNewAccessToken = IssueAdminNewAccessToken(adminRepository)
+
+const authController = AuthController(createAdmin, loginAdmin, logoutAdmin, issueAdminNewAccessToken);
 
 const router = express.Router();
 
-router.post('/login', asyncHandler(authController.loginUser))
-router.post('/register', asyncHandler(authController.registerUser))
+router.post('/login', asyncHandler(authController.handleAdminLogin))
+router.post('/logout', asyncHandler(authController.handleAdminLogout))
+router.post('/register', asyncHandler(authController.handleAdminRegister))
+router.post('/refresh-token', asyncHandler(authController.handleAdminRefreshToken))
 
 export default router
