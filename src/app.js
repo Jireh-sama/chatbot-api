f13b3662cbcf370092a5676e1d1a234df8eb8158
Jireh <jirehtumbagahan@gmail.com>
2024-10-10 18:13:29 +0800

    refactor: update cors origin

--- src/app.js
+++ src/app.js
@@ -6,19 +6,20 @@ import { knowledgeRoutes, authRoutes, chatbotRoutes } from './interface/routes/i
 import { globalErrorHandler } from './interface/middleware/errorHandler.js';
 import cookieParser from 'cookie-parser';
 import { verifyToken } from './interface/middleware/authMiddleware.js';
+
 const port = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(cookieParser())
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://cob-chatbot.vercel.app', 'http://localhost:3001'],
+  origin: ['http://localhost:5173', 'https://cob-chatbot.vercel.app', 'http://localhost:3001', 'https://chatbot-adminpanel.vercel.app'],
   credentials: true,
 }));
 app.use(helmet())
 
-app.use(express.static('public'));
 
 app.use('/api/auth', authRoutes)
 app.use('/api/knowledge', verifyToken, knowledgeRoutes)
