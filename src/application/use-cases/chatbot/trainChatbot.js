f03304f3e09f9d8a666d838cc0a442402b2b4091
Jireh <jirehtumbagahan@gmail.com>
2024-10-19 20:15:05 +0800

    refactor: add code to delete the nlp model file when training chatbot

--- src/application/use-cases/chatbot/trainChatbot.js
+++ src/application/use-cases/chatbot/trainChatbot.js
@@ -1,5 +1,10 @@
+import fs from 'fs/promises';
+import { modelFilePath } from '#src/infrastructure/config/paths.js';
 function trainChatbot(chatbot){
   const execute = async () => {
+    await fs.unlink(modelFilePath, (err) => {
+      if (err) throw err
+    })
     await chatbot.train()
   }
   return {execute}
