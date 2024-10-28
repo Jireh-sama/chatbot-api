3b108e205283bca9d6334ab4cf4025596888351f
Jireh <jirehtumbagahan@gmail.com>
2024-10-28 20:38:55 +0800

    refactor: update service account path

--- src/infrastructure/persistence/database/firebase/firebase-config.js
+++ src/infrastructure/persistence/database/firebase/firebase-config.js
@@ -1,10 +1,14 @@
-
 import admin from 'firebase-admin';
 import { readFile } from 'fs/promises';
 
 
+// const serviceAccount = JSON.parse(
+  //   await readFile(new URL('./serviceAccount/cob-chatbot-firebase-adminsdk-ix45m-d65aeb395d.json', import.meta.url))
+  // );
+  
+const serviceAccountPath = '/etc/secrets/cob-chatbot-firebase-adminsdk-ix45m-d65aeb395d.json';
 const serviceAccount = JSON.parse(
-  await readFile(new URL('./serviceAccount/cob-chatbot-firebase-adminsdk-ix45m-d65aeb395d.json', import.meta.url))
+  await readFile(serviceAccountPath, 'utf-8')
 );
 
 admin.initializeApp({
