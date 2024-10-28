6e5332a698a7d79840091c4afc0f7b5a2be6b357
Jireh <jirehtumbagahan@gmail.com>
2024-10-28 20:17:24 +0800

    refactor: update train route and add /frequency route

--- src/interface/routes/chatbotRoutes.js
+++ src/interface/routes/chatbotRoutes.js
@@ -1,22 +1,32 @@
 import { verifyToken, verifyAPIKey } from "../middleware/authMiddleware.js";
 import { asyncHandler } from "../middleware/errorHandler.js";
+import cron from 'node-cron'
 
 import express from "express";
 import ChatbotController from "../controllers/chatbotController.js";
-import { chatbotClient } from "#src/infrastructure/service/index.js";
+import { chatbotClient, knowledgeRepository } from "#src/infrastructure/service/index.js";
 
 import ProcessUserQuery from "#src/application/use-cases/chatbot/processUserQuery.js";
 import TrainChatbot from "#src/application/use-cases/chatbot/trainChatbot.js";
+import UpdateKnowledgeEntryFrequency from "#src/application/use-cases/knowledge/update/updateKnowledgeEntryFrequency.js";
+import UpdateQuestionFrequency from "../../application/use-cases/chatbot/updateQuestionFrequency.js";
 
 const router = express.Router()
 
 const processUserQuery = ProcessUserQuery(chatbotClient);
 const trainChatbot = TrainChatbot(chatbotClient);
+const updateKnowledgeEntryFrequency = UpdateKnowledgeEntryFrequency(knowledgeRepository);
+const updateQuestionFrequency = UpdateQuestionFrequency(knowledgeRepository);
 
-const chatBotController = ChatbotController(processUserQuery, trainChatbot);
+const chatBotController = ChatbotController(processUserQuery, trainChatbot, updateKnowledgeEntryFrequency, updateQuestionFrequency);
   
+// cron.schedule('*/5 * * * * *', () => {
+//   console.log('rawr');
+// })
+
 router.post('/query', verifyAPIKey, asyncHandler(chatBotController.processUserQuery))
 router.post('/query-admin', verifyToken, asyncHandler(chatBotController.processUserQuery))
-router.post('/train', verifyToken, asyncHandler(chatBotController.trainChatbot))
+router.post('/train', asyncHandler(chatBotController.trainChatbot))
+router.post('/frequency', asyncHandler(chatBotController.updateQuestionFrequency))
 
 export default router
