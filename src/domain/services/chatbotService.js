e217dba028c84d35645b7f61a12cc2d337386a3a
Jireh <jirehtumbagahan@gmail.com>
2024-10-30 11:17:23 +0800

    refactor: use export and import when saving and loading model data

--- src/domain/services/chatbotService.js
+++ src/domain/services/chatbotService.js
@@ -4,19 +4,15 @@
  * library and exposes a clean API to the rest of the application
  */
 
-function chatbotService(chatbot, reader, modelFilePath) {
+function chatbotService(chatbot) {
 
   const processQuery = async (query) => {
     const response = await chatbot.process('en', query)
     return response;
   }
 
-  const loadModel = async () => {
-    const modelData = await reader(modelFilePath)
-    if(!modelData) {
-      throw new Error('Modal data not found')
-    }
-    chatbot.load(modelFilePath)
+  const loadModel = async (modelData) => {
+    chatbot.import(modelData);
   }
 
   const loadEntry = (knowledgeEntry) => {
@@ -33,14 +29,11 @@ function chatbotService(chatbot, reader, modelFilePath) {
     chatbot.addAnswer("en", intent, `${answer} [${fileUrl}]`);
   };
   
-  const saveModel = async (modelFilePath) => {
-    try {
-      await chatbot.train()
-      chatbot.save(modelFilePath)
-      
-    } catch (error) {
-      console.log(`Error occured while saving model: ${error}`);
-    }
+  const saveModel = async () => {
+    await chatbot.train()
+    const isMinified = true
+    const modelData = chatbot.export(isMinified)
+    return modelData
   }
 
   return { processQuery, loadModel, saveModel, loadEntry}
