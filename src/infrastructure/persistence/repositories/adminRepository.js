95222f60eb3f4faa2a319357c86adeca0823c691
Jireh <jirehtumbagahan@gmail.com>
2024-11-07 00:44:11 +0800

    refactor: update admin repository to include creating, deleting and finding all admin

--- src/infrastructure/persistence/repositories/adminRepository.js
+++ src/infrastructure/persistence/repositories/adminRepository.js
@@ -4,23 +4,26 @@ function adminRepository(db) {
     return await db.readCollection(filter, null, true)
   }
 
-  const findAllAdmin = async (email) => {
-    const query = { email }
-    const isSingle = true
-    return await db.readCollection(query, null, isSingle)
+  const findAllAdmin = async () => {
+    const projection = { refreshToken: 0 }
+    return await db.readCollection({}, projection)
   }
-  const createAdmin = async (email, password) => {
-    const document = { email, password }
+  const createAdmin = async (email, password, role) => {
+    const document = { email, password, role }
     await db.addDocument(document)
+  }
+  const deleteAdmin = async (id) => {
+    const filter = { _id: id }
+    return await db.deleteDocument(filter)
   }
   const updateAdminRefreshToken = async (adminId, query) => {
     await db.updateDocument({ _id: adminId }, query)
   } 
-  
   return {
     findOneAdmin,
     findAllAdmin,
     createAdmin,
+    deleteAdmin,
     updateAdminRefreshToken,
   }
 }
