144308c9db0afb26bbe337007b57b5cbd048e1f8
Jireh <jirehtumbagahan@gmail.com>
2024-11-07 00:43:32 +0800

    refactor: update variable name to adminRecord

--- src/application/use-cases/admin/loginAdmin.js
+++ src/application/use-cases/admin/loginAdmin.js
@@ -5,27 +5,28 @@ import { ACCESS_TOKEN_EXPIRATION_TIME, REFRESH_TOKEN_EXPIRATION_TIME } from "#sr
 function loginAdmin(adminRepository) {
   const execute = async (email, password) => {
 
-    const foundAdmin = await adminRepository.findOneAdmin({ email })
+    const adminRecord = await adminRepository.findOneAdmin({ email })
     
-    if (!foundAdmin) {
+    if (!adminRecord) {
       throw new CustomError('Invalid credentials', 404)
     }
 
-    const isPasswordValid = await verifyPassword(password, foundAdmin.password)
+    const isPasswordValid = await verifyPassword(password, adminRecord.password)
     
     if (!isPasswordValid) {
       throw new CustomError('Invalid credentials', 404)
     }
 
-    const accessToken = generateAccessToken({ id: foundAdmin._id}, process.env.ACCESS_TOKEN_SECRET, ACCESS_TOKEN_EXPIRATION_TIME)
-    const refreshToken = generateAccessToken({ id: foundAdmin._id}, process.env.REFRESH_TOKEN_SECRET, REFRESH_TOKEN_EXPIRATION_TIME)
+    const accessToken = generateAccessToken({ id: adminRecord._id}, process.env.ACCESS_TOKEN_SECRET, ACCESS_TOKEN_EXPIRATION_TIME)
+    const refreshToken = generateAccessToken({ id: adminRecord._id}, process.env.REFRESH_TOKEN_SECRET, REFRESH_TOKEN_EXPIRATION_TIME)
     
-    await adminRepository.updateAdminRefreshToken(foundAdmin._id, { $set: { refreshToken } } )
-    if (process.env.NODE_ENV === 'development') {
-      console.log('Successfully logged in admin:', foundAdmin);
-    }
+    await adminRepository.updateAdminRefreshToken(adminRecord._id, { $set: { refreshToken } } )
 
-    return {accessToken, refreshToken}
+    if (process.env.NODE_ENV === 'development') {
+      console.log('Successfully logged in admin:', adminRecord);
+    }
+    const adminInfo = { email: adminRecord.email, role: adminRecord.role, id: adminRecord._id  }
+    return {accessToken, refreshToken, adminInfo}
   }
   return {execute}
 }
