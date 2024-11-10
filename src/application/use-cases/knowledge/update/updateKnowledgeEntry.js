3bdcd7e9031dfc2d55a1287df0f846594bc6d8a0
Jireh <jirehtumbagahan@gmail.com>
2024-11-10 14:51:11 +0800

    refactor: add checking when updating knowledge entry

--- src/application/use-cases/knowledge/update/updateKnowledgeEntry.js
+++ src/application/use-cases/knowledge/update/updateKnowledgeEntry.js
@@ -3,6 +3,8 @@ import createKnowledgeEntry from "#src/domain/entities/knowledgeEntry.js"
 function updateKnowledgeEntry(knowledgeRepository) {
 
   const execute = async (knowledgeEntryIntent, updatedKnowledgeEntry) => {
+    const result = await knowledgeRepository.getKnowledgeEntry(knowledgeEntryIntent);
+    if (!result) throw new CustomError(`No knowledge entry found with the intent: ${knowledgeEntryIntent}`, 404)
 
     const { intent, documents, answer, fileUrl } = updatedKnowledgeEntry;
     const newKnowledgeEntry = createKnowledgeEntry(intent, documents, answer, fileUrl)
