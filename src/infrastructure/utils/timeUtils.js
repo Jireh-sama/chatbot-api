aa31f792e29b78839864726f0053dab5ab1f1cc2
Jireh <jirehtumbagahan@gmail.com>
2024-11-10 15:13:10 +0800

    refactor: set timezone to Asia/Manila

--- src/infrastructure/utils/timeUtils.js
+++ src/infrastructure/utils/timeUtils.js
@@ -1,5 +1,7 @@
 export const getCurrentTime = () => {
   const now = new Date();
+  
+  // Ensure that the Date is properly adjusted for the Philippines time zone
   const options = {
     year: 'numeric',
     month: '2-digit',
@@ -8,7 +10,8 @@ export const getCurrentTime = () => {
     minute: '2-digit',
     second: '2-digit',
     hour12: true,
-    timeZone: 'Asia/Manila'
+    timeZone: 'Asia/Manila', // Set to the Philippines timezone
   };
+
   return now.toLocaleString('en-US', options);
 }
