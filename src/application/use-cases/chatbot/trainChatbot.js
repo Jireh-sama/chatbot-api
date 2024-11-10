611c066345ce3d83ac42fd993ab9d1786bb413f9
Jireh <jirehtumbagahan@gmail.com>
2024-11-10 14:50:36 +0800

    chore: remove comments

--- src/application/use-cases/chatbot/trainChatbot.js
+++ src/application/use-cases/chatbot/trainChatbot.js
@@ -1,13 +1,8 @@
-import fs from 'fs/promises';
-import { modelFilePath } from '#src/infrastructure/config/paths.js';
 import { getCurrentTime } from '#src/infrastructure/utils/timeUtils.js';
 import { insertDocument } from '#src/infrastructure/persistence/database/firebase/client.js';
 
 function trainChatbot(chatbot){
   const execute = async () => {
-    // await fs.unlink(modelFilePath, (err) => {
-    //   if (err) throw err
-    // })
     await chatbot.train()
     const timeFinishedTraining = getCurrentTime()
     await insertDocument('profile', { timeFinishedTraining })
