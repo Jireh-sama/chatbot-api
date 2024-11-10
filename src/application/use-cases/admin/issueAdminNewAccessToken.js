3cc1e70ef6324b07cfbfa3fda4ddddb35c9493c4
Jireh <jirehtumbagahan@gmail.com>
2024-11-10 14:50:16 +0800

    refactor: add logging for easier debugging

--- src/application/use-cases/admin/issueAdminNewAccessToken.js
+++ src/application/use-cases/admin/issueAdminNewAccessToken.js
@@ -3,10 +3,11 @@ import { generateAccessToken } from "#src/infrastructure/utils/tokenUtils.js"
 
 function issueAdminNewAccessToken(adminRepository) {
   const execute = async (refreshToken) => {
-
+  
     const foundAdmin = await adminRepository.findOneAdmin({ refreshToken })
 
     if (!foundAdmin) {
+      console.log('No admin found with this refresh token: ', refreshToken);
       throw new CustomError('No admin found with the given credentials', 404)
     }
     console.log('generating new access token for admin');
