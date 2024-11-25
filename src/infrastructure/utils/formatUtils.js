6741dfd2c553edb7e0c7cf9e1d608a4d0f8d69d9
Jireh <jirehtumbagahan@gmail.com>
2024-11-25 11:46:38 +0800

    chore: add utility function related to formatting

---
+++ src/infrastructure/utils/formatUtils.js
@@ -0,0 +1,39 @@
+import { removeStopwords, eng } from 'stopword'
+
+export const extractFileNameFromUrl = (fileUrl) => {
+  try {
+    // Decode the URL to handle %2F and other encoded characters
+    const decodedUrl = decodeURIComponent(fileUrl);
+    
+    // Extract the part after the last '/'
+    const startIndex = decodedUrl.lastIndexOf("/") + 1;
+    
+    // Find the end of the filename (before ?alt=)
+    const endIndex = decodedUrl.indexOf("?alt=");
+    
+    if (startIndex !== -1 && endIndex !== -1) {
+      return decodedUrl.substring(startIndex, endIndex);
+    }
+    
+    return null; // Return null if no valid filename is found
+  } catch (error) {
+    console.error("Failed to extract filename:", error);
+    return null;
+  }
+}
+
+export const removeStopWords = (text) => {
+  // Default English stopwords
+  const stopwords = eng;
+
+  // Adding exceptions
+  const exceptions = ["get"];
+  const customStopwords = stopwords.filter(
+    (word) => !exceptions.includes(word)
+  );
+
+  const oldString = text.split(" ");
+  const newString = removeStopwords(oldString, customStopwords);
+
+  return newString.join(' ')
+}
