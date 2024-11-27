25cb4af30ea8d318e4aa13a9738958c8e8c3edd2
Jireh <jirehtumbagahan@gmail.com>
2024-11-27 22:07:07 +0800

    refactor: remove frequency tracking

--- src/interface/controllers/chatbotController.js
+++ src/interface/controllers/chatbotController.js
@@ -1,25 +1,27 @@
-function chatbotController(processUserQueryUseCase, trainChatbotUseCase, updateKnowledgeEntryFrequencyUseCase, updateQuestionFrequencyUseCase) {
-  
+function chatbotController(
+  processUserQueryUseCase,
+  trainChatbotUseCase
+) {
   const processUserQuery = async (req, res) => {
     const { userQuery } = req.body;
-    const response = await processUserQueryUseCase.execute(userQuery)
+    const response = await processUserQueryUseCase.execute(userQuery);
 
     // await updateKnowledgeEntryFrequencyUseCase.execute(response.intent)
-    res.status(200).json({ success: true, response })
-  }
+    res.status(200).json({ success: true, response });
+  };
 
   const trainChatbot = async (_req, res) => {
-    const timeFinishedTraining = await trainChatbotUseCase.execute()
-    console.log('Model trained successfully');
-    res.status(200).json({ success: true, response: `Model trained successfully: ${timeFinishedTraining}` })
-  }
+    const timeFinishedTraining = await trainChatbotUseCase.execute();
+    console.log("Model trained successfully");
+    res
+      .status(200)
+      .json({
+        success: true,
+        response: `Model trained successfully: ${timeFinishedTraining}`,
+      });
+  };
 
-  const updateQuestionFrequency = async (_req, res) => {
-    await updateQuestionFrequencyUseCase.execute()
-    res.status(200).json({ success: true, response: `Updated question frequency` })
-  }
-
-  return { processUserQuery, trainChatbot, updateQuestionFrequency }
+  return { processUserQuery, trainChatbot };
 }
 
-export default chatbotController
+export default chatbotController;
