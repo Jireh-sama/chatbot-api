fa5ba8373b84dcd032421cbfa246c45077f1fdef
Jireh <jirehtumbagahan@gmail.com>
2024-11-27 21:18:13 +0800

    refactor: add archive routes and code cleanup

--- src/app.js
+++ src/app.js
@@ -1,109 +1,69 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-// import helmet from 'helmet';
-import { knowledgeRoutes, authRoutes, chatbotRoutes } from './interface/routes/index.js';
-import { globalErrorHandler } from './interface/middleware/errorHandler.js';
-import cookieParser from 'cookie-parser';
-// import { rateLimit } from 'express-rate-limit'
-import { verifyToken } from './interface/middleware/authMiddleware.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import {
+  knowledgeRoutes,
+  authRoutes,
+  chatbotRoutes,
+  archiveRoutes,
+} from "./interface/routes/index.js";
+import { globalErrorHandler } from "./interface/middleware/errorHandler.js";
+import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+import { verifyToken } from "./interface/middleware/authMiddleware.js";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const publicPath = path.join(__dirname, '..', 'public');
+const publicPath = path.join(__dirname, "..", "public");
 const port = process.env.PORT || 3001;
 const app = express();
 
-// const WHITELIST_URL = ["https://chatbot-api-0zup.onrender.com", "http://localhost:5173"]
-
-// app.use(helmet.contentSecurityPolicy({
-//   directives: {
-//     defaultSrc: ["'self'"],
-//     connectSrc: [
-//       "'self'",
-//       "https://firestore.googleapis.com",
-//       "https://firebasestorage.googleapis.com",
-//       "https://firebase.googleapis.com",
-//       "https://firebaseinstallations.googleapis.com",
-//       "https://identitytoolkit.googleapis.com",
-//       "https://chatbot-api-0zup.onrender.com",
-//       "https://securetoken.googleapis.com",
-//     ],
-//     scriptSrc: [
-//       "'self'",
-//       "'unsafe-inline'",
-//       "https://www.gstatic.com",
-//       "https://www.googleapis.com",
-//       "https://www.googleapis.com/auth",
-//       "https://*.firebaseio.com",
-//     ],
-//     styleSrc: [
-//       "'self'",
-//       "https://fonts.googleapis.com",
-//     ],
-//     imgSrc: [
-//       "'self'",
-//       "https://*.firebaseio.com",
-//       "https://firebasestorage.googleapis.com",
-//       "data:",  // Allow data URIs for images
-//     ],
-//     fontSrc: [
-//       "'self'",
-//       "https://fonts.gstatic.com",
-//     ],
-//   },
-// }));
-
-app.use(cors({
-  origin: ['http://localhost:5173', 'https://cob-chatbot.vercel.app', 'http://localhost:3001', 'https://chatbot-adminpanel.vercel.app', 'https://www.bacoorchatbot.com'],
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://cob-chatbot.vercel.app",
+      "http://localhost:3001",
+      "https://chatbot-adminpanel.vercel.app",
+      "https://www.bacoorchatbot.com",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(bodyParser.json());
-app.use(cookieParser())
-
-// const limiter = rateLimit({
-//   windowMs: 15 * 60 * 1000, // 15 minutes
-//   limit: 100,
-//   message: 'Too many requests, please try again later.'
-// })
-// app.use((req, res, next) => {
-//   if (WHITELIST_URL.includes(req.headers.origin)) {
-//     return next()
-//   }
-//   limiter(req, res, next)
-// })
+app.use(cookieParser());
 
 // API routes
-app.use('/api/auth', authRoutes)
-app.use('/api/knowledge', verifyToken, knowledgeRoutes)
-app.use('/api/chatbot', chatbotRoutes)
-
-app.get('/', (_, res) => {
-  res.redirect(301, '/user');
-})
+app.use("/api/auth", authRoutes);
+app.use("/api/knowledge", verifyToken, knowledgeRoutes);
+app.use("/api/chatbot", chatbotRoutes);
+app.use("/api/archive", archiveRoutes);
+app.get("/", (_, res) => {
+  res.redirect(301, "/user");
+});
 
 // Static File routes
-app.use('/user', express.static(path.join(publicPath, 'user')));
-app.get('/user/*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'user', 'index.html'));
-});
-app.use('/admin', express.static(path.join(publicPath, 'admin')));
-app.get('/admin/*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'admin', 'index.html'));
+app.use("/user", express.static(path.join(publicPath, "user")));
+app.get("/user/*", (req, res) => {
+  res.sendFile(path.join(publicPath, "user", "index.html"));
 });
 
-app.use(globalErrorHandler)
+app.use("/admin", express.static(path.join(publicPath, "admin")));
+app.get("/admin/*", (req, res) => {
+  res.sendFile(path.join(publicPath, "admin", "index.html"));
+});
+
+app.use(globalErrorHandler);
 
 const startServer = () => {
   app.listen(port, () => {
-    if (process.env.NODE_ENV === 'development') {
+    if (process.env.NODE_ENV === "development") {
       console.log(`Server is running at http://localhost:${port}`);
       return;
     }
     console.log(`Server is running on port: ${port}`);
   });
-}
+};
 
 export default startServer;
