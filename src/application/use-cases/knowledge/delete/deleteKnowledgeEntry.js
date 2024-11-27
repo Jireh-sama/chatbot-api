836e382462cb165aade742d7bad63b55f9b8229d
Jireh <jirehtumbagahan@gmail.com>
2024-11-27 21:28:03 +0800

    refactor: add function to move deleted knowledge enty to archive collection

--- src/application/use-cases/knowledge/delete/deleteKnowledgeEntry.js
+++ src/application/use-cases/knowledge/delete/deleteKnowledgeEntry.js
@@ -1,18 +1,42 @@
-function deleteKnowledgeEntry(knowledgeRepository) {
+import { ObjectId } from "mongodb";
+
+function deleteKnowledgeEntry(archiveRepository, knowledgeRepository) {
 
   const execute = async (knowledgeBase, knowledgeEntryIntent) => {
   
-    const existingKnowledge = await knowledgeRepository.getKnowledgeBase(
+    const targetKnowledgeBase = await knowledgeRepository.getKnowledgeBase(
       knowledgeBase
     );
+    if (!targetKnowledgeBase) {
+      throw new CustomError(`Knowledge Base ${knowledgeBase} not found`, 404);
+    }
 
-    if (!existingKnowledge) throw new CustomError(`Knowledge base ${knowledgeBase} does not exist`, 404)
+    if (targetKnowledgeBase.knowledgeEntry.length === 1) {
+      throw new CustomError(`Unable to delete knowledge entry, because the associated Knowledge Base cannot be empty`, 400)
+    }
 
-    const intentExist = existingKnowledge.knowledgeEntry.some((entry => entry.intent === knowledgeEntryIntent))
+    const targetKnowledgeEntry = targetKnowledgeBase.knowledgeEntry.filter((entry => entry.intent === knowledgeEntryIntent))
+    console.log(targetKnowledgeEntry);
+    if (!targetKnowledgeEntry || targetKnowledgeEntry.length === 0) {
+      throw new CustomError(`Knowledge entry with intent ${knowledgeEntryIntent} does not exist on ${knowledgeBase}`, 404)
+    }
+    
 
-    if (!intentExist) throw new CustomError(`Knowledge entry with intent ${knowledgeEntryIntent} does not exist`, 404)
-      
-    await knowledgeRepository.deleteKnowledgeEntry(knowledgeBase, knowledgeEntryIntent)
+    const knowledgeEntryDocument = {
+      _id: new ObjectId(),
+      archivedAt: new Date(),
+      knowledgeBase: targetKnowledgeBase.knowledgeBase,
+      knowledgeEntry: targetKnowledgeEntry[0],
+    };
+
+    await Promise.all([
+      knowledgeRepository.deleteKnowledgeEntry(
+        knowledgeBase,
+        knowledgeEntryIntent
+      ),
+      archiveRepository.archiveData("knowledgeEntries", knowledgeEntryDocument),
+    ]);
+    // await 
   }
 
   return { execute }
