acac75e5ec2c9c80a79c9f9e4e30b6a06f21fd70
Jireh <jirehtumbagahan@gmail.com>
2024-11-27 22:06:37 +0800

    chore: code cleanup and format

--- src/interface/routes/chatbotRoutes.js
+++ src/interface/routes/chatbotRoutes.js
@@ -1,32 +1,33 @@
 import { verifyToken, verifyAPIKey } from "../middleware/authMiddleware.js";
 import { asyncHandler } from "../middleware/errorHandler.js";
-// import cron from 'node-cron'
 
 import express from "express";
 import ChatbotController from "../controllers/chatbotController.js";
-import { chatbotClient, knowledgeRepository } from "#src/infrastructure/service/index.js";
-
+import { chatbotClient } from "#src/infrastructure/service/index.js";
 import ProcessUserQuery from "#src/application/use-cases/chatbot/processUserQuery.js";
 import TrainChatbot from "#src/application/use-cases/chatbot/trainChatbot.js";
-import UpdateKnowledgeEntryFrequency from "#src/application/use-cases/knowledge/update/updateKnowledgeEntryFrequency.js";
-import UpdateQuestionFrequency from "../../application/use-cases/chatbot/updateQuestionFrequency.js";
 
-const router = express.Router()
+const router = express.Router();
 
 const processUserQuery = ProcessUserQuery(chatbotClient);
 const trainChatbot = TrainChatbot(chatbotClient);
-const updateKnowledgeEntryFrequency = UpdateKnowledgeEntryFrequency(knowledgeRepository);
-const updateQuestionFrequency = UpdateQuestionFrequency(knowledgeRepository);
 
-const chatBotController = ChatbotController(processUserQuery, trainChatbot, updateKnowledgeEntryFrequency, updateQuestionFrequency);
-  
-// cron.schedule('*/5 * * * * *', () => {
-//   console.log('rawr');
-// })
+const chatBotController = ChatbotController(processUserQuery, trainChatbot);
 
-router.post('/query', verifyAPIKey, asyncHandler(chatBotController.processUserQuery))
-router.post('/query-admin', verifyToken, asyncHandler(chatBotController.processUserQuery))
-router.post('/train', asyncHandler(chatBotController.trainChatbot))
-router.post('/frequency', asyncHandler(chatBotController.updateQuestionFrequency))
+router.post(
+  "/query",
+  verifyAPIKey,
+  asyncHandler(chatBotController.processUserQuery)
+);
+router.post(
+  "/query-admin",
+  verifyToken,
+  asyncHandler(chatBotController.processUserQuery)
+);
+router.post(
+  "/train",
+  verifyToken,
+  asyncHandler(chatBotController.trainChatbot)
+);
 
-export default router
+export default router;
