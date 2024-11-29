9ca8e0969c1d513111fd5875b9053c0324396bb9
Jireh <jirehtumbagahan@gmail.com>
2024-11-29 13:10:03 +0800

    refactor: initialize cron tasks on app start

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import 'dotenv/config'
 import startServer from'./app.js';
 import { chatbotClient } from './infrastructure/service/index.js';
 import CustomError from './infrastructure/errors/CustomError.js';
+import { initializeCronTasks } from './infrastructure/utils/cronUtils.js';
 
 global.CustomError = CustomError;
 
 const initialize = async () => {
     await chatbotClient.initialize()
     startServer();
+    initializeCronTasks();
 }
 
 initialize().catch(error => {
