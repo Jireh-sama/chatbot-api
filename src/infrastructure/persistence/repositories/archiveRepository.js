0672e93da9930aec230e34cf3e22a84f229dfdd4
Jireh <jirehtumbagahan@gmail.com>
2024-11-29 13:09:05 +0800

    refactor: add functions for retrieving and deleting old archives

--- src/infrastructure/persistence/repositories/archiveRepository.js
+++ src/infrastructure/persistence/repositories/archiveRepository.js
@@ -14,15 +14,33 @@ function archiveRepository(db) {
     return await db.readCollection(query, filter, isSingle)
   }
 
-  const removeKnowledgeArchive= async (type, id) => {
+  const removeKnowledgeArchive = async (type, id) => {
     const query = { type }
     const updateData = { $pull: { items: { _id: id } } }
     await db.updateDocument(query, updateData)
   };
 
+  const removeOldArchives = async (type, expirationDate) => {
+    const query = { type }
+    const updateData = { $pull: { items: { archivedAt: { $lt: expirationDate } } } }
 
+    return await db.updateDocuments(query, updateData)
+  }
 
-  return { archiveData, getArchiveDocument, removeKnowledgeArchive }
+  const getOldArchives = async (type) => {
+    const thirtyDaysAgo = new Date();
+    thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
+
+    const query = { 
+      type,
+      "items.archivedAt": { $lt: thirtyDaysAgo }
+    };
+
+    const filter = { _id: 0 };
+    return await db.readCollection(query, filter);
+  };
+
+  return { archiveData, getArchiveDocument, removeKnowledgeArchive, removeOldArchives, getOldArchives }
 }
 
 export default archiveRepository
