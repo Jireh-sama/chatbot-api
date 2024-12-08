47174c87ae0ca8131657d75676420fcdcd778b49
Jireh <jirehtumbagahan@gmail.com>
2024-12-08 15:07:24 +0800

    refactor: refactor: when generating a new access token for admin verify the refresh token first

--- src/application/use-cases/admin/issueAdminNewAccessToken.js
+++ src/application/use-cases/admin/issueAdminNewAccessToken.js
@@ -1,5 +1,7 @@
 import { ACCESS_TOKEN_EXPIRATION_TIME } from "#src/infrastructure/config/token.js";
 import { generateAccessToken } from "#src/infrastructure/utils/tokenUtils.js"
+import jwt from "jsonwebtoken";
+import { logMessage } from "../../../infrastructure/utils/loggingUtils.js";
 
 function issueAdminNewAccessToken(adminRepository) {
   const execute = async (refreshToken) => {
@@ -7,9 +9,17 @@ function issueAdminNewAccessToken(adminRepository) {
     const foundAdmin = await adminRepository.findOneAdmin({ refreshToken })
 
     if (!foundAdmin) {
-      console.log('No admin found with this refresh token: ', refreshToken);
-      throw new CustomError('No admin found with the given credentials', 404)
+      logMessage(`No admin found with this refresh token: ${refreshToken}`);
+      throw new CustomError("No admin found with the given credentials", 404);
     }
+
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err) => {
+      if (err) {
+        logMessage("Refresh Token is invalid or expired");
+        throw new CustomError("Unauthorized", 401);
+      }
+    });
+    
     console.log('generating new access token for admin');
     return await generateAccessToken(foundAdmin, process.env.ACCESS_TOKEN_SECRET, ACCESS_TOKEN_EXPIRATION_TIME)
   }
