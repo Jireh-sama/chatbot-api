68f258018aaf14ccd246453702aea1db51ac7897
Jireh <jirehtumbagahan@gmail.com>
2025-01-11 00:43:10 +0800

    refactor: remove the model save behaviour when training the chatbot

--- src/infrastructure/persistence/chatbot/client.js
+++ src/infrastructure/persistence/chatbot/client.js
@@ -45,9 +45,11 @@ function chatbotClient(config, knowledgeRepository, modelRepository) {
         );
       }
 
+      console.log('Model Training Successful');
+      
       // Save model data to model dir
-      const modelData = await chatbotService.saveModel();
-      await modelRepository.updateModelData(modelData)
+      // const modelData = await chatbotService.saveModel();
+      // await modelRepository.updateModelData(modelData)
     } catch (error) {
       console.log("Training error:", error.message || error);
     }
